df5feaa7b3518cc73bacc446ec0167287f03dc93
Oleh Tythynsky <videoblane@gmail.com>
2016-05-23 21:28:47 +0300

    Delete sql_expert.sql

--- ["SQL", "Scripts"]/sql_expert.sql
+++
@@ -1,8 +0,0 @@
-use smart;
-SELECT * FROM smart.expert;
-insert into expert(first_name,last_name,user_id)
-values ('Андрій','Павельчак',NULL);
-insert into expert(first_name,last_name,user_id)
-values ('Зеновій','Верес',NULL);
-insert into expert(first_name,last_name,user_id)
-values ('Юрій','Яцук',NULL)
